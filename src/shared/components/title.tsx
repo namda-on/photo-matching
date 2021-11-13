6ffaa2be140118529e9006ef031c9644e2b5e217
namda-on <jinyoung1997@gmail.com>
2021-11-13 17:59:09 +0900

    feat : title component 추가

---
+++ src/shared/components/title.tsx
@@ -0,0 +1,15 @@
+import react from "react";
+
+interface TitleProps {
+  name: string;
+}
+
+const Title = ({ name }: TitleProps) => {
+  return (
+    <div className="flex w-20 h-10 my-2 items-center justify-start">
+      <div className="w-15 h-8"> {name}</div>
+    </div>
+  );
+};
+
+export default Title;
