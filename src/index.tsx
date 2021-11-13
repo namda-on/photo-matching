5c327417a7fdff604f2d3176b9acee0a067abf8e
namda-on <jinyoung1997@gmail.com>
2021-11-13 17:57:50 +0900

    feat : router 설정

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import ReactDOM from "react-dom";
+import "./shared/global.css";
+import SearchPage from "./pages/search";
+import MainPage from "./pages/main";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/search">
+          <SearchPage />
+        </Route>
+        <Route path="/">
+          <MainPage />
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<App />, document.getElementById("root"));
